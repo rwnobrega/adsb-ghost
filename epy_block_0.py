a5ebe67a0b3ea15fa20098799660a43e2cbe3223
Roberto Nobrega <rwnobrega@gmail.com>
2016-10-13 12:36:21 -0300

    Clean epy_block_0.py [2]

--- epy_block_0.py
+++ epy_block_0.py
@@ -22,7 +22,6 @@ class blk(gr.interp_block):  # other base classes are basic_block, decim_block, 
         )
 
     def work(self, input_items, output_items):
-        print(output_items)
         for i, b in enumerate(input_items[0]):
             output_items[0][2*i] = b
             output_items[0][2*i + 1] = not b
