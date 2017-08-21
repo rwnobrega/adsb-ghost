f79022d1acfef1c9f332b7e3491fd058c0c08b41
lucas lucindo vieira <llucindov@Anas-MacBook-Pro.local>
2017-08-21 15:19:47 -0300

    Added new functions

---
+++ encoder.py
@@ -0,0 +1,106 @@
+import math, time
+import adsb_dec
+import util
+
+latz = 15
+
+def aircraft_altitude(altitude):
+    alt = (altitude + 1000)/25
+    alt_bin = bin(alt)
+    altitude = alt_bin.split("0b")
+    altitude = altitude[1]
+    altitude_1 = altitude[0:7] + '1' + altitude[8:12]
+    if len(altitude) is not 12:
+        while len(altitude) < 12:
+            altitude = '0' + altitude
+    return altitude
+
+def aircraft_id(icao, data):
+    """Aircraft ID
+
+    Args:
+        icao (6-character hexstring): ICAO24 aircraft type
+        data (8-character hexstring): aircraft ID.
+
+    Returns:
+    ADS-B packet: 28-character hexstring
+
+    """
+    chars = '#ABCDEFGHIJKLMNOPQRSTUVWXYZ#####_###############0123456789######'
+    dfca = '8D'
+    data_int_list = [chars.index(x) for x in data]
+    data_bin = ''.join('{0:06b}'.format(x) for x in data_int_list)
+    data_hex = '{0:012X}'.format(int(data_bin, 2))
+    msg0 = dfca + icao + '20' + data_hex
+    crc = '{0:06X}'.format(int(util.crc(msg0 + '000000', encode=True), 2))
+    return msg0 + crc
+
+def aircraft_position(TC_SS_NICsb, ALT, LAT_CPR, LON_CPR, flag, icao):
+    dfca = '8D'
+    ALT = ALT
+    T = '0'
+    F = flag
+    LAT_CPR = LAT_CPR
+    LON_CPR = LON_CPR
+    package =  '0b' + TC_SS_NICsb + ALT + T + F + LAT_CPR + LON_CPR
+    package = hex(int(package, 2))
+    package = package.split("0x")
+    package = package[1]
+    msg = dfca + icao + package
+    crc = '{0:06X}'.format(int(util.crc(msg + '000000', encode=True), 2))
+    return msg + crc
+
+def get_lat(coordenadas):
+    latitude_bin = bin(coordenadas[0])
+    latitude = latitude_bin.split("0b")
+    latitude = latitude[1]
+    if len(latitude) is not 17:
+        while len(latitude) < 17:
+            latitude = '0' + latitude
+    return latitude
+
+def get_long(coordenadas):
+    longitude_bin = bin(coordenadas[1])
+    longitude = longitude_bin.split("0b")
+    longitude = longitude[1]
+    if len(longitude) is not 17:
+        while len(longitude) < 17:
+            longitude= '0' + longitude
+    return longitude
+
+def nz(ctype):
+    return 4 * latz - ctype
+
+def dlat(ctype):
+	nzcalc = nz(ctype)
+	if nzcalc == 0:
+		return 360.0
+	else:
+		return 360.0 / nzcalc
+
+def nl(declat_in):
+	if abs(declat_in) >= 87.0:
+		return 1.0
+	return math.floor( (2.0*math.pi) * math.acos(1.0- (1.0-math.cos(math.pi/(2.0*latz))) / math.cos( (math.pi/180.0)*abs(declat_in) )**2 )**-1)
+
+def dlon(declat_in, ctype):
+	nlcalc = max(nl(declat_in)-ctype, 1)
+	return 360.0 / nlcalc
+
+def cpr_encode(lat, lon, ctype, surface):
+	if surface is True:
+		scalar = 2.**19
+	else:
+		scalar = 2.**17
+
+	dlati = dlat(ctype)
+	yz = math.floor(scalar * ((lat % dlati)/dlati) + 0.5)
+	rlat = dlati * ((yz / scalar) + math.floor(lat / dlati))
+
+	dloni = dlon(lat, ctype)
+	xz = math.floor(scalar * ((lon % dloni)/dloni) + 0.5)
+
+	yz = int(yz) & (2**17-1)
+	xz = int(xz) & (2**17-1)
+
+	return (yz, xz) #lat, lon
---
+++ flying_dutch.py
@@ -0,0 +1,94 @@
+import json
+import encoder
+import adsb_dec
+import gmplot
+from termcolor import colored
+from geopy.geocoders import Nominatim
+
+
+MOVEMENT_CONST = 0.0001
+icao = "40621D"
+city = "Florianopolis"
+alts = '110000111000'
+route = []
+geolocator = Nominatim()
+
+# def sender(identifier, **payload_args):
+#     return json.dumps([{'identifier': identifier, 'payload': payload_args}])
+#     make_request(message_identifiers['call_transmit_request'],
+#                         promise_id=promise_id,
+#                         target=target,
+#                         modules=modules)
+
+def travel(destination, alt):
+    global city
+    global icao
+    location = geolocator.geocode(destination, timeout=10)
+    print("Voce quer ir: %s" % location.address)
+    print("Latitude de destino: %s" % location.latitude)
+    print("Longitude de destino:  %s" % location.longitude)
+
+    position_end = {"latitude" : location.latitude,
+                    "longitude": location.longitude}
+
+    lat_list = []
+    lon_list = []
+
+    lat_list.append(position_init["latitude"])
+    lon_list.append(position_init["longitude"])
+    lat_list.append(position_init["latitude"] + MOVEMENT_CONST)
+    lon_list.append(position_init["longitude"] + MOVEMENT_CONST)
+
+    lat_list.append(position_end["latitude"])
+    lon_list.append(position_end["longitude"])
+    lat_list.append(position_end["latitude"] + MOVEMENT_CONST)
+    lon_list.append(position_end["longitude"] + MOVEMENT_CONST)
+
+    gmap = gmplot.GoogleMapPlotter(-27.5973002, -48.5496098, 10)
+    gmap.plot(lat_list, lon_list, 'cornflowerblue', edge_width=10)
+    gmap.draw("map.html")
+    i = 0
+    while i < len(lat_list):
+        coordenadas = encoder.cpr_encode(lat_list[i], lon_list[i], 0, 1)
+        latitude = encoder.get_lat(coordenadas)
+        longitude = encoder.get_long(coordenadas)
+        message_position = encoder.aircraft_position('01011000', alt, latitude, longitude, '0', icao)
+        coordenadas = encoder.cpr_encode(lat_list[i+1], lon_list[i+1], 1, 1)
+        latitude = encoder.get_lat(coordenadas)
+        longitude = encoder.get_long(coordenadas)
+        message_position1 = encoder.aircraft_position('01011000', alt, latitude, longitude, '1', icao)
+        print colored(message_position.upper(), 'green')
+        print colored(message_position1.upper(), 'red')
+        coordenadas_dec = adsb_dec.position(message_position, message_position1, 1, 11)
+        print coordenadas_dec
+        i = i + 2
+    city = destination
+
+while True:
+    location = geolocator.geocode(city, timeout=10)
+    print("Voce esta em: %s" % location.address)
+    print("Sua atual latitude: %s" % location.latitude)
+    print("Sua atual longitude:  %s" % location.longitude)
+    print("Sua atual altitude:  %s" % alts)
+
+    position_init = {"latitude" : location.latitude,
+                     "longitude": location.longitude}
+
+    print "\n"
+    print "travel - Set a travel command"
+    print "name - change name"
+    print "alt - Set altitude"
+    print "exit - exit flying dutch\n\n"
+    command = raw_input("What do you want to do? \n")
+    if command == 'travel':
+        destination = raw_input("Where u want to go? \n")
+        travel(destination, alts)
+    if command == 'name':
+        name = raw_input("What is the name? \n")
+        message = encoder.aircraft_id(icao, 'Flying_Dutch')
+    if command == 'alt':
+        alt = raw_input("What is the new alt? \n")
+        alts = encoder.aircraft_altitude(int(alt))
+        travel(city, alts)
+    if command == 'exit':
+        exit()
