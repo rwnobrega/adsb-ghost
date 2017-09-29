0fe87eb4885259c1a2565e6a4a98b7f93a4e92ee
Lucas Lucindo Vieira <lucas.lucindo@byne.com.br>
2017-09-29 00:55:44 -0300

    Now, flying dutch read a flight_plan and generated a flight_plan_binary containing binary packets. So, it's posible to use with GNU Radio.

--- flying_dutch.py
+++ flying_dutch.py
@@ -1,32 +1,31 @@
+# -*- coding: UTF-8 -*-
 import json
 import encoder
 import adsb_dec
 import gmplot
+import util
 from termcolor import colored
 from geopy.geocoders import Nominatim
 
 
 MOVEMENT_CONST = 0.0001
 icao = "40621D"
-city = "Florianopolis"
+city = "Aeroporto Hercilio Luz - Florianopolis"
 alts = '110000111000'
 route = []
 geolocator = Nominatim()
-
-# def sender(identifier, **payload_args):
-#     return json.dumps([{'identifier': identifier, 'payload': payload_args}])
-#     make_request(message_identifiers['call_transmit_request'],
-#                         promise_id=promise_id,
-#                         target=target,
-#                         modules=modules)
+plan = open('flight_plan_binary.txt', 'w+')
+lock = True
+lat_list_plot = []
+lon_list_plot = []
 
 def travel(destination, alt):
     global city
     global icao
+    global plan
+    global lat_list_plot
+    global lon_list_plot
     location = geolocator.geocode(destination, timeout=10)
-    print("Voce quer ir: %s" % location.address)
-    print("Latitude de destino: %s" % location.latitude)
-    print("Longitude de destino:  %s" % location.longitude)
 
     position_end = {"latitude" : location.latitude,
                     "longitude": location.longitude}
@@ -44,52 +43,62 @@ def travel(destination, alt):
     lat_list.append(position_end["latitude"] + MOVEMENT_CONST)
     lon_list.append(position_end["longitude"] + MOVEMENT_CONST)
 
-    gmap = gmplot.GoogleMapPlotter(-27.5973002, -48.5496098, 10)
-    gmap.plot(lat_list, lon_list, 'cornflowerblue', edge_width=10)
-    gmap.draw("map.html")
+    lat_list_plot.append(position_init["latitude"])
+    lon_list_plot.append(position_init["longitude"])
+    lat_list_plot.append(position_init["latitude"] + MOVEMENT_CONST)
+    lon_list_plot.append(position_init["longitude"] + MOVEMENT_CONST)
+
+    lat_list_plot.append(position_end["latitude"])
+    lon_list_plot.append(position_end["longitude"])
+    lat_list_plot.append(position_end["latitude"] + MOVEMENT_CONST)
+    lon_list_plot.append(position_end["longitude"] + MOVEMENT_CONST)
+
     i = 0
     while i < len(lat_list):
         coordenadas = encoder.cpr_encode(lat_list[i], lon_list[i], 0, 1)
         latitude = encoder.get_lat(coordenadas)
         longitude = encoder.get_long(coordenadas)
-        message_position = encoder.aircraft_position('01011000', alt, latitude, longitude, '0', icao)
+        message_position = encoder.aircraft_position('01011000', alt, latitude,
+                                                     longitude, '0', icao)
         coordenadas = encoder.cpr_encode(lat_list[i+1], lon_list[i+1], 1, 1)
         latitude = encoder.get_lat(coordenadas)
         longitude = encoder.get_long(coordenadas)
-        message_position1 = encoder.aircraft_position('01011000', alt, latitude, longitude, '1', icao)
-        print colored(message_position.upper(), 'green')
-        print colored(message_position1.upper(), 'red')
-        coordenadas_dec = adsb_dec.position(message_position, message_position1, 1, 11)
-        print coordenadas_dec
+        message_position1 = encoder.aircraft_position('01011000', alt, latitude,
+                                                      longitude, '1', icao)
+        plan.write(util.hex2bin(message_position))
+        plan.write(util.hex2bin(message_position1))
+        coordenadas_dec = adsb_dec.position(message_position,
+                                            message_position1, 1, 11)
         i = i + 2
     city = destination
 
-while True:
-    location = geolocator.geocode(city, timeout=10)
-    print("Voce esta em: %s" % location.address)
-    print("Sua atual latitude: %s" % location.latitude)
-    print("Sua atual longitude:  %s" % location.longitude)
-    print("Sua atual altitude:  %s" % alts)
 
-    position_init = {"latitude" : location.latitude,
-                     "longitude": location.longitude}
+flight_plan = open("flight_plan.txt","r")
+a = flight_plan.readlines()
+list_command = []
+list_value = []
+for i in range(len(a)):
+    b = a[i].split("-")
+    list_command.append(b[0])
+    list_value.append(b[1].rstrip())
 
-    print "\n"
-    print "travel - Set a travel command"
-    print "name - change name"
-    print "alt - Set altitude"
-    print "exit - exit flying dutch\n\n"
-    command = raw_input("What do you want to do? \n")
-    if command == 'travel':
-        destination = raw_input("Where u want to go? \n")
-        travel(destination, alts)
-    if command == 'name':
-        name = raw_input("What is the name? \n")
-        message = encoder.aircraft_id(icao, 'Flying_Dutch')
-    if command == 'alt':
-        alt = raw_input("What is the new alt? \n")
-        print 'AQUI 1'
-        alts = encoder.aircraft_altitude(int(alt))
-        travel(city, alts)
-    if command == 'exit':
-        exit()
+while lock == True:
+
+    for i in range(len(a)):
+        location = geolocator.geocode(city, timeout=10)
+        position_init = {"latitude" : location.latitude,
+                         "longitude": location.longitude}
+        if i == (len(a) - 1):
+            lock = False
+        if list_command[i] == 'travel':
+            travel(list_value[i], alts)
+        if list_command[i] == 'name':
+            message = encoder.aircraft_id(icao, 'Flying_Dutch')
+        if list_command[i] == 'alt':
+            alts = encoder.aircraft_altitude(int(list_value[i]))
+            travel(city, alts)
+
+gmap = gmplot.GoogleMapPlotter(-27.5973002, -48.5496098, 10)
+gmap.plot(lat_list_plot, lon_list_plot, 'cornflowerblue', edge_width=5)
+gmap.draw("trajetoria.html")
+plan.close()
