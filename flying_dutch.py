7a35e9f9f240b3bfb502eb3b63b3b7ca0163487f
Lucas Lucindo Vieira <lucas.lucindo@byne.com.br>
2017-10-09 10:34:12 -0300

    Modified the way of read files. Getting flight plan from dictionary and using latitude and longitude

--- flying_dutch.py
+++ flying_dutch.py
@@ -10,7 +10,8 @@ from geopy.geocoders import Nominatim
 
 MOVEMENT_CONST = 0.0001
 icao = "40621D"
-city = "Aeroporto Hercilio Luz - Florianopolis"
+lat_atual = -27.670118
+lon_atual = -48.5481544
 alts = '110000111000'
 route = []
 geolocator = Nominatim()
@@ -19,16 +20,20 @@ lock = True
 lat_list_plot = []
 lon_list_plot = []
 
-def travel(destination, alt):
+def travel(destination, alt, latitude=None, longitude=None):
     global city
     global icao
     global plan
     global lat_list_plot
     global lon_list_plot
-    location = geolocator.geocode(destination, timeout=10)
 
-    position_end = {"latitude" : location.latitude,
-                    "longitude": location.longitude}
+    if (latitude == None and longitude == None):
+        location = geolocator.geocode(destination, timeout=10)
+        position_end = {"latitude" : location.latitude,
+                        "longitude": location.longitude}
+    else:
+        position_end = {"latitude" : latitude,
+                        "longitude": longitude}
 
     lat_list = []
     lon_list = []
@@ -69,37 +74,49 @@ def travel(destination, alt):
         plan.write(util.hex2bin(message_position1))
         coordenadas_dec = adsb_dec.position(message_position,
                                             message_position1, 1, 11)
+        lat_atual = lat_list[i]
+        lon_atual = lon_list[i]
         i = i + 2
-    city = destination
+    print "LATITUDE ATUAL"
+    print lat_atual
+    print "LONGITUDE ATUAL"
+    print lon_atual
 
-
-flight_plan = open("flight_plan.txt","r")
-a = flight_plan.readlines()
 list_command = []
 list_value = []
-for i in range(len(a)):
-    b = a[i].split("-")
-    list_command.append(b[0])
-    list_value.append(b[1].rstrip())
+dicts_from_file = []
+with open('flight_plan.txt','r') as inf:
+    for line in inf:
+        dicts_from_file.append(eval(line))
 
 while lock == True:
 
-    for i in range(len(a)):
-        location = geolocator.geocode(city, timeout=10)
-        position_init = {"latitude" : location.latitude,
-                         "longitude": location.longitude}
-        if i == (len(a) - 1):
+    for i in range(len(dicts_from_file)):
+        position_init = {"latitude" : lat_atual,
+                         "longitude": lon_atual}
+
+        command = str(dicts_from_file[i].get('command'))
+        command = command[2:len(command)-2]
+        if i == (len(dicts_from_file) - 1):
             lock = False
-        if list_command[i] == 'travel':
-            travel(list_value[i], alts)
-        if list_command[i] == 'name':
+
+        if command == 'travel':
+            latitude = str(dicts_from_file[i].get('latitude'))
+            latitude = latitude[1:len(latitude)-2]
+            longitude = str(dicts_from_file[i].get('longitude'))
+            longitude = longitude[1:len(longitude)-2]
+            travel(None, alts, float(latitude), float(longitude))
+        if command == 'name':
             message = encoder.aircraft_id(icao, 'Flying_Dutch')
-        if list_command[i] == 'alt':
-            alts = encoder.aircraft_altitude(int(list_value[i]))
+        if command == 'alt':
+            altura = str(dicts_from_file[i].get('altura'))
+            altura = altura[1:len(altura)-1]
+            print altura
+            alts = encoder.aircraft_altitude(int(altura))
             travel(city, alts)
 
 gmap = gmplot.GoogleMapPlotter(-27.5973002, -48.5496098, 10)
-gmap.plot(lat_list_plot, lon_list_plot, 'cornflowerblue', edge_width=5)
+gmap.plot(lat_list_plot, lon_list_plot, 'cornflowerblue', edge_width=10)
 gmap.draw("trajetoria.html")
 plan.close()
 
